c47c1e9b6ac5028d85fd171517f6860b7a340f5b
TimmosQuadros <timm.rasmussen@gmail.com>
2016-09-15 14:37:45 +0200

    Done!

---
+++ src/main/java/entity/DiscountType.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+/**
+ * DTYPE
+ * The idea of DTYPE column generated in the DB is two choose which inherited class to use.
+ * In other words each customer can have its own discount type.
+ * 
+ *
+ * @author TimmosQuadros
+ */
+
+@Inheritance(strategy=InheritanceType.JOINED)
+@Entity
+public class DiscountType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public double calcDiscount(double pricePerItem,int Quantity){
+        return pricePerItem*Quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof DiscountType)) {
+            return false;
+        }
+        DiscountType other = (DiscountType) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.DiscountType[ id=" + id + " ]";
+    }
+    
+}
